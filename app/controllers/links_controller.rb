c52213dcc85cca0b4150d605695ec8c3bb7b3f2a
Javier Noris <norisjavier@gmail.com>
2014-03-30 15:28:39 -0700

    Getting all link actions to work

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,54 @@
+class LinksController < ApplicationController
+
+	def index
+      @link = Link.all
+	end
+
+	def show
+	  @link = Link.find(params[:id])
+	end
+
+	def new
+      @link = Link.new
+	end
+
+	def create
+	  @link = Link.new(link_params)
+		if @link.save
+			redirect_to @link 
+		else
+			render action: 'new'
+		end
+	end
+
+	def destroy
+	  @link = Link.find(params[:id])
+	  if @link.destroy
+	  	redirect_to action: 'index'
+	  else
+	  	render action: 'show'
+	  end
+	end
+
+	def edit
+	  @link = Link.find(params[:id])
+	end
+
+
+
+	def update
+	@link = Link.find params[:id]
+      if @link.update(link_params)
+      	redirect_to @link 
+      else
+        render action: 'edit'
+      end
+	end
+
+private
+    def link_params
+      params.require(:link).permit(:id, :url, :title)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RedditOnRails2::Application.routes.draw do
   devise_for :users
   resources :pages
+  resources :links
   root :to => "pages#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
